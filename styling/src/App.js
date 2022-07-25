68cb7b13f3281fa3e29ad8a725b35cc2170c72e0
KDH <kdh0824@disec.kr>
2022-07-25 17:32:50 +0900

    Styled-Components 예제

--- styling/src/App.js
+++ styling/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import StyledComponentsExample from "./components/StyleComponentsExample/StyledComponentsExample";
 
 function App() {
     return (
         <div className="App">
-
+            <StyledComponentsExample />
         </div>
     );
 }
---
+++ styling/src/components/StyleComponentsExample/StyledComponentsExample.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function StyledComponetsExample() {
+    const Title = styled.h1`
+        font-size: 1.5em;
+        text-align: center;
+        color: palevioletred;
+    `
+
+    const Wrapper = styled.section`
+        padding: 4em;
+        background: papayawhip;
+    `
+
+    return (
+        <Wrapper>
+            <Title>Hello World!</Title>
+        </Wrapper>
+    )
+}
